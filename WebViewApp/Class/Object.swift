26075bece49313abf7a4f3d0f70db47b3e8557a9
dima2483432@gmail.com <dima2483432@gmail.com>
2025-04-07 21:36:43 +0300

    добавил еще предмет в коллекцию

--- WebViewApp/Class/Object.swift
+++ WebViewApp/Class/Object.swift
@@ -17,4 +17,5 @@ struct Object {
 
 let objects = [Object(name: "Наушники Airpods pro max", images: UIImage(named: "Max1")!, cost: "449 $"),
                Object(name: "Ноутбук Macbook Air", images: UIImage(named: "macbook1")!, cost: "799 $"),
-               Object(name: "Наушники Airpods 3rd", images: UIImage(named: "airpods3rd1")!, cost: "199 $")]
+               Object(name: "Наушники Airpods 3rd", images: UIImage(named: "airpods3rd1")!, cost: "199 $"),
+               Object(name: "IPhone 16e", images: UIImage(named: "iphone141")!, cost: "399 $")]
