71366be96be22f4fc1845e7b5d900e7e908cd787
dima2483432@gmail.com <dima2483432@gmail.com>
2025-04-07 20:25:37 +0300

    Initial Commit

---
+++ WebViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebViewApp
+//
+//  Created by Александра Аришина on 07.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
