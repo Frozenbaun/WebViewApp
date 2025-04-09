b0d698b217bea9c01d15fb93cf48ac4d16209b33
dima2483432@gmail.com <dima2483432@gmail.com>
2025-04-09 19:56:48 +0300

    тест успешен, будем сливать ветки

--- WebViewApp/Cells/ImageCollectionViewCell.swift
+++ WebViewApp/Cells/ImageCollectionViewCell.swift
@@ -9,9 +9,40 @@ import UIKit
 
 class ImageCollectionViewCell: UICollectionViewCell {
     
-    @IBOutlet weak var imageView: UIImageView!
+    let imageView = UIImageView()
     
-    func setup(obj: Object, indexPath: IndexPath) {
-        imageView.image = obj.images[indexPath.row]
+    override init(frame: CGRect) {
+        
+        super.init(frame: frame)
+        setupCell()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupCell() {
+        self.contentView.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        
+        self.clipsToBounds = true
+        
+        NSLayoutConstraint.activate([
+            
+            
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+        
+        imageView.contentMode = .scaleAspectFit
+    }
+    
+    func setupImage(obj: Object, indexPath: IndexPath) {
+        
+        imageView.image = obj.images[indexPath.item]
+
     }
 }
--- WebViewApp/Controllers/ObjectViewController.swift
+++ WebViewApp/Controllers/ObjectViewController.swift
@@ -10,10 +10,9 @@ import UIKit
 class ObjectViewController: UIViewController {
 
     var object: Object?
+//    let cell = ImageCollectionViewCell(frame: CGRect(x: 0, y: 0, width: 240, height: 240))
+    var collectionView: UICollectionView!
     
-    
-   
-    @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var costLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
     
@@ -23,6 +22,7 @@ class ObjectViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupCollectionView()
         startView()
         
     }
@@ -36,8 +36,28 @@ class ObjectViewController: UIViewController {
         self.nameLabel.text = object.name
         self.costLabel.text = object.cost
         
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+      
+    }
+    
+    func setupCollectionView() {
+        collectionView = UICollectionView(frame: .init(x: 80, y: 250, width: 250, height: 250) , collectionViewLayout: setupFlowLayout())
+        collectionView.backgroundColor = UIColor.white
+        collectionView.isPagingEnabled = true
+        view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        collectionView.register(ImageCollectionViewCell.self, forCellWithReuseIdentifier: "imageCell")
         
         
+    }
+    
+    func setupFlowLayout() -> UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = .init(width: 240, height: 240)
+        layout.scrollDirection = .horizontal
+        return layout
     }
 
 }
@@ -50,10 +70,10 @@ extension ObjectViewController: UICollectionViewDelegate, UICollectionViewDataSo
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageCell", for: indexPath) as! ImageCollectionViewCell
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageCell", for: indexPath) as? ImageCollectionViewCell else {return UICollectionViewCell()}
             guard let obj = object else {return cell}
-            cell.setup(obj: obj, indexPath: indexPath)
-            print("srabotal")
+        cell.setupImage(obj: obj, indexPath: indexPath)
+            
             return cell
     }
     
